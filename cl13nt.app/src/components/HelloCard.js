dcc7cf6a5f1cf4b5caf0b8f1b353e4561bc969f5
noumecha spaker <noumechaivan@gmail.com>
2023-05-03 22:01:41 +0100

    upd : setup the firestore configuration

--- cl13nt.app/src/components/HelloCard.js
+++ cl13nt.app/src/components/HelloCard.js
@@ -1,8 +1,35 @@
+/* eslint-disable no-unused-vars */
 import { Card, CardContent, Typography } from "@mui/material";
 import { ReactComponent as WelcomeSvg } from "../assets/welcome.svg";
 import { SvgContainer } from "./SvgContainer";
+import { useEffect, useState } from "react";
+import { collection, query, onSnapshot } from "firebase/firestore";
+import { firestore } from "../firebase/firebase";
 
 const HelloCard = () => {
+
+  const [info, setInfo] = useState([])
+  const [ids, setIds] = useState([])
+  useEffect(() => {
+    const getData = async () => {
+      const data = await query(collection(firestore, "users_accnt"));
+      console.log("data: " + JSON.stringify(data));
+
+      onSnapshot(data, (QuerySnapshot) => {
+        const databaseInfo = [];
+        const dataIds = [];
+
+        QuerySnapshot.forEach((doc) => {
+          databaseInfo.push(doc.data().usersAccnt);
+          dataIds.push(doc.id);
+        });
+        setIds(dataIds);
+        setInfo(databaseInfo);
+      });
+    }
+    getData()
+  }, []);
+
   return (
     <Card elevation={0} sx={{ backgroundColor: "transparent", mb: 2 }}>
       <CardContent>
--- cl13nt.app/src/firebase/firebase.js
+++ cl13nt.app/src/firebase/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import {getFirestore} from "@firebase/firestore"
+//import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -8,14 +9,16 @@ import { getAnalytics } from "firebase/analytics";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_apiKey,
-  authDomain: "transport-service-plus.firebaseapp.com",
-  projectId: "transport-service-plus",
-  storageBucket: "transport-service-plus.appspot.com",
-  messagingSenderId: "929704715460",
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
   appId: process.env.REACT_APP_appId,
-  measurementId: "G-RNP28P9VVW"
+  measurementId: process.env.REACT_APP_measurementId
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//const analytics = getAnalytics(app);
+
+export const firestore = getFirestore(app)
